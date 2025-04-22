21e79478706beaa3a8b5965a9936837f6d8ac2b0
Tripchanski <arturtripking01@gmail.com>
2025-04-22 09:41:16 +0300

    first commit

---
+++ constants/MenuImages.ts
@@ -0,0 +1,27 @@
+import { ImageSourcePropType } from "react-native";
+
+import espresso from "@/assets/images/menu/espresso.png";
+import americano from "@/assets/images/menu/americano.png";
+import latte from "@/assets/images/menu/latte.png";
+import cappuccino from "@/assets/images/menu/cappuccino.png";
+import macchiato from "@/assets/images/menu/macchiato.png";
+import mocha from "@/assets/images/menu/mocha.png";
+import flatwhite from "@/assets/images/menu/flatwhite.png";
+import cortado from "@/assets/images/menu/cortado.png";
+import coldbrew from "@/assets/images/menu/coldbrew.png";
+import affogato from "@/assets/images/menu/affogato.png";
+
+const menuImages: ImageSourcePropType[] = [
+  espresso,
+  americano,
+  latte,
+  cappuccino,
+  macchiato,
+  mocha,
+  flatwhite,
+  cortado,
+  coldbrew,
+  affogato,
+];
+
+export default menuImages;
